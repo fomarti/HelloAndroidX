65533de9dbbc2eefbe9f79d147da9f54782f6a8b
fomarti <fomarti@gmail.com>
2019-05-17 09:55:21 -0500

    Without date and time update bug fix

--- app/src/main/java/aplimovil/com/helloandroidx/MainActivity.java
+++ app/src/main/java/aplimovil/com/helloandroidx/MainActivity.java
@@ -27,6 +27,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
-        //updateTime();
+        updateTime();
     }
 }
